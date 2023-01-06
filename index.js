96c27189ebffec8ddab6be406d24287e2a699620
Toznyy <anthony.cormier29860@gmail.com>
2023-01-06 11:58:45 +0000

    ajout de la pause t'as capté ma couille

--- index.js
+++ index.js
@@ -21,9 +21,11 @@ exercice2();
 function exercice3(){
     let ingredients = ["Salade", "Tomate", "Oignon"];
     let button = document.getElementById("btn-add-ingredients");
+    let ul=document.querySelector("ul:first-of-type");
     button.addEventListener("click", function(event){
         for (let i = 0; i < ingredients.length; i++){
             let li = document.createElement("li");
+            ul.appendChild(li);
             let txt = document.createTextNode(ingredients[i]);
             li.appendChild(txt);
         }
@@ -32,29 +34,58 @@ function exercice3(){
 
 exercice3()
 
-function exercice4(){
-    let heroes = [
-        {
-            name : "Clark Kent",
-            secret : "Superman"
+function exercice4() {
+    let heroes = [{
+            name: "Clark Kent",
+            secret: "Superman"
         },
         {
-            name : "Diana Prince",
-            secret : "Wonder Woman"
+            name: "Diana Prince",
+            secret: "Wonder Woman"
         },
         {
-            name : "Tony Stark",
+            name: "Tony Stark",
             secret: "Iron Man"
         }
     ];
+    let button = document.getElementById("btn-add-hero");
+    let tbody = document.querySelector("tbody");
+    let count = 0;
+    button.addEventListener("click", function(event) {
+        
+        if (count < heroes.length){
+            let tdName = document.createElement("td");
+            let tdSecret = document.createElement("td");
+            let tr = document.createElement("tr");
+            tr.appendChild(tdName);
+            tr.appendChild(tdSecret);
+            tbody.appendChild(tr);
+            let txtName = document.createTextNode(heroes[count].name);
+            let txtSecret = document.createTextNode(heroes[count].secret);
+            tdName.appendChild(txtName);
+            tdSecret.appendChild(txtSecret);
+            tbody.appendChild(tr);
+            count++;
+        }
+    });
 }
 
-function exercice5()
-{
+exercice4();
 
+function exercice5(){
+    let button = document.getElementById("btn-turn");
+    let box1 = document.getElementById("blue");
+    let box2 = document.getElementById("red");
+    let box3 = document.getElementById("green");
+    button.addEventListener("click", function(event){
+        box1.style.backgroundColor = "red";
+        box2.style.backgroundColor = "green";
+        box3.style.backgroundColor = "blue";
+    });
 }
 
-function exercice6()
-{
+exercice5();
+
+function exercice6(){
 
 }
